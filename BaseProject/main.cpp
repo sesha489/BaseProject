ed89974b093ced4ed2f55f4450e0734c6481e651
Seshadri <sesha489ele@gmail.com>
2025-05-02 19:37:30 +0530

    Update main.cpp

--- BaseProject/main.cpp
+++ BaseProject/main.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+void startScreen(void) {
+ int option;
+ option = 0;
+
+ cout << "Select your modbus application" << endl;
+ cout << "1. Client" << endl;
+ cout << "2. Server" << endl;
+ cout << "3. Exit" << endl;
+ cin >> option;
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
+ startScreen();
 	return 0;
 }

6d0163c53b5ecfab615743fc2e518308dab15c3f
Seshadri <sesha489ele@gmail.com>
2025-05-08 22:48:12 +0530

    Update main.cpp
    Added selection function

--- BaseProject/main.cpp
+++ BaseProject/main.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+void ModServer(){
+	cout << "Modbus server" << endl;
+}
+
+void ModClient(){
+	cout << "Modbus client" << endl;
+}
+
+void Selection(int optionSel)
+{
+	cin>>selecOp;
+	switch(selecOp){
+		case 1:
+			system("cls");
+			ModServer();
+			break;
+		case 2:
+			system("cls");
+			ModClient();
+			break;
+		case 3:
+			system("cls");
+			cout << "Exit" << endl;
+			break;
+		default:
+			cout<<"Not a valid option. Enter a valid option."<<endl;
+			Selection();
+	}
+}
+
 void startScreen(void) {
 	int option;
 	option = 0;
@@ -11,9 +41,12 @@ void startScreen(void) {
 	cout << "2. Server" << endl;
 	cout << "3. Exit" << endl;
 	cin >> option;
+
+	Selection(option);
 }
 
 int main() {
 	startScreen();
+	std::cin.get();
 	return 0;
 }

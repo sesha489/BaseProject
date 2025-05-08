5e6ec7f020e64a411debb81e64fa7f6437b4e37a
Seshadri <sesha489ele@gmail.com>
2025-05-08 22:36:11 +0530

    Indent correction
    Indent correction in main.cpp by Seshadri

--- BaseProject/main.cpp
+++ BaseProject/main.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 void startScreen(void) {
- int option;
- option = 0;
+	int option;
+	option = 0;
 
- cout << "Select your modbus application" << endl;
- cout << "1. Client" << endl;
- cout << "2. Server" << endl;
- cout << "3. Exit" << endl;
- cin >> option;
+	cout << "Select your modbus application" << endl;
+	cout << "1. Client" << endl;
+	cout << "2. Server" << endl;
+	cout << "3. Exit" << endl;
+	cin >> option;
 }
 
 int main() {
- startScreen();
+	startScreen();
 	return 0;
 }
